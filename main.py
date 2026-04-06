4af144134507c13b4499f7d5ad3b93ac0b71dd6c
Tharun-06 <tharuntomy2004@gmail.com>
2026-04-06 17:22:58 +0530

    minor changes

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
